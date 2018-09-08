0b1f9a8859fad8d494e7d8203a34d4ff82d7856f
BuildTools <unconfigured@null.spigotmc.org>
2018-09-08 00:25:28 -0400

    midnight btw

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,20 @@
 package com.company;
+import java.io.*;
+import java.sql.SQLOutput;
+import java.util.*;
+
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	Scanner kbReader = new Scanner(System.in);
+
+	System.out.println("What is your first name? ");
+	String firstName = kbReader.nextLine();
+
+	System.out.println("What is your last name? ");
+	String lastName = kbReader.nextLine();
+
+	System.out.println("Your full name is " + firstName + " " + lastName + ".");
     }
 }
